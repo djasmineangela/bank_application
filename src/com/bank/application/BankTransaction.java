1e880e17109b2192fead6fa96bef033a0d2fe0f3
nitzy21 <59757658+nitzy21@users.noreply.github.com>
2022-11-01 00:41:06 +0800

    Update BankTransaction.java

--- src/com/bank/application/BankTransaction.java
+++ src/com/bank/application/BankTransaction.java
@@ -1,5 +1,9 @@
 package com.bank.application;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 import com.bank.util.TransactionValidator;
@@ -7,6 +11,7 @@ import com.bank.util.TransactionValidator;
 public class BankTransaction extends TransactionValidator implements Validator {
     BankAccount bankAccount;
     BankDetailsFileReader bankDetailsFileReader;
+    BankDetailsFileWriter bankDetailsFileWriter;
     TransactionList transactionList;
 
     public BankTransaction() {
@@ -45,9 +50,50 @@ public class BankTransaction extends TransactionValidator implements Validator {
             bankAccount.setBalance(totalBalance);
             System.out.println("Total Balance: " + totalBalance);
         }
-        
+
         for (String trans : transactionList.getTransactions()) {
             System.out.println(trans);
         }
+    }
+
+    public void getStatement() {
+        // bankDetailsFileWriter = new BankDetailsFileWriter();
+        // bankDetailsFileWriter.displayStatement();
+
+        // for (String transaction : transactionList.getTransactions()) {
+        //// bufferedWriter.write(transaction + "\n");
+        // System.out.println(transaction);
+        // }
+
+        String fileName = "C:\\Users\\parnit\\Downloads\\bank_application-main\\bank_application-main\\src\\bankFiles\\bankStatement.txt";
+//        String fileName = "D:\\workspace\\BankApplication\\src\\bankFiles\\bankStatement.txt";
+        
+        File myFile = new File(fileName); 
+
+        FileWriter fileWriter;
+        BufferedWriter bufferedWriter;
+
+        try {
+
+            if (myFile.exists()) {
+                myFile.delete();
+            }
+
+            myFile.createNewFile();
+            fileWriter = new FileWriter(fileName, true);
+            bufferedWriter = new BufferedWriter(fileWriter);
+           
+            for (String transaction : transactionList.getTransactions()) {
+                bufferedWriter.write("\n" + transaction);
+                System.out.println(transaction);
+            }
+
+            
+            bufferedWriter.close();
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
     }
 }
