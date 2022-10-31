80423a7881c5e381ebd23b878621014c9ec09677
nitzy21 <59757658+nitzy21@users.noreply.github.com>
2022-11-01 00:39:49 +0800

    Update BankDetailsFileWriter.java

--- src/com/bank/application/BankDetailsFileWriter.java
+++ src/com/bank/application/BankDetailsFileWriter.java
@@ -1,5 +1,36 @@
 package com.bank.application;
 
-public class BankDetailsFileWriter {
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
 
+public class BankDetailsFileWriter {
+    
+    TransactionList list;
+
+    public void displayStatement() {
+        
+       list = new TransactionList();
+
+        try {
+//            FileWriter fileWriter = new FileWriter("C:\\Users\\parnit\\Downloads\\bank_application-main\\bank_application-main\\src\\bankFiles\\bankStatement.txt", true);
+            
+            FileWriter fileWriter = new FileWriter("C:\\Users\\parnit\\Documents\\trial.txt", true);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            for (String transaction : list.getTransactions()) {
+                bufferedWriter.write(transaction);
+                System.out.println(transaction);
+            }
+
+            // bufferedWriter.write(list.transactions.toString());
+            // bufferedWriter.write("4,John,Doe\n");
+
+            bufferedWriter.close();
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
